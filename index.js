4e6c0a73830fc123f63faa66341444583f9695e2
10969torrexx <pablitojrtorre@gmail.com>
2025-08-09 14:43:59 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+const isDev = !app.isPackaged;
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  });
+
+  if (isDev) {
+    win.loadURL('http://localhost:5173'); // Vite dev server
+  } else {
+    win.loadFile(path.join(__dirname, 'renderer/dist/index.html'));
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
