59930977037b82b67996a0d19ebbad88b9607388
10969torrexx <pablitojrtorre@gmail.com>
2025-08-23 21:53:28 +0800

    pablito-dev: showing user counts

--- renderer/src/pages/Dashboard.jsx
+++ renderer/src/pages/Dashboard.jsx
@@ -1,17 +1,39 @@
 import {useEffect, useState} from 'react'
 import { UserRoles } from '../enums/userRoles';
-import { ChangeCredentials, SideBar, HorizontalBar } from '@components';
+import { ChangeCredentials, SideBar, HorizontalBar, UserCounts } from '@components';
 import { UsersManagement } from '@pages';
 import { toast } from 'react-toastify';
+import { AuthServices } from '@services';
 
 export default function Dashboard() {
   const [userData, setUserData] = useState(null); 
   const [showChangePassword, setShowChangePassword] = useState(true);
+  const [staffCount, setStaffCount] = useState(0);
+  const [supervisorCount, setSupervisorCount] = useState(0);
+
   useEffect(() => {
     const storedUserData = localStorage.getItem('user');
     if (storedUserData) {
       setUserData(JSON.parse(storedUserData));
     }
+
+    //TODO: creating a list of user counts by role
+    const getAllUsers = async () => {
+      try {
+        const response = await AuthServices.getAllUsers(JSON.parse(storedUserData).id);
+        if (response.success) {
+          const users = response.data;
+          console.log('Fetched users:', users);
+          setStaffCount(users.filter(user => user.role === UserRoles.STAFF).length);
+          setSupervisorCount(users.filter(user => user.role === UserRoles.SUPERVISOR).length);
+        } else {
+          toast.error(response.message || "Failed to fetch user counts");
+        }
+      } catch (error) {
+        console.error('Error fetching users:', error);
+      }
+    };
+    getAllUsers();
   }, []);
 
   //TODO: handling side bar open / close state
@@ -39,6 +61,10 @@ export default function Dashboard() {
         >
           <div className='content'>
             <h1 className='text-2xl font-semibold mb-4'>Dashboard</h1>
+            <div className="flex flex-row flex-wrap gap-4">
+              <UserCounts role={UserRoles.SUPERVISOR} userCounts={supervisorCount} status="active"/>
+              <UserCounts role={UserRoles.STAFF} userCounts={staffCount} status="inactive"/>
+            </div>
           </div>
         </div>
       </div>
