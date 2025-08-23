9967eaa2c39a7e4b1bb0137d28cbda8a920afe37
10969torrexx <pablitojrtorre@gmail.com>
2025-08-24 01:31:05 +0800

    pablito-dev: minor design fix for the button

--- renderer/src/pages/Document.jsx
+++ renderer/src/pages/Document.jsx
@@ -22,18 +22,18 @@ export default function Document() {
                 <div className="p-4 flex-1 flex flex-col w-screen transition-all duration-300">
                     <h2 className="text-lg font-semibold text-left mb-4">Document Title</h2>
                     <div className="flex justify-left space-x-6 p-6">
-                        <div className="bg-white shadow-lg rounded-lg p-6 flex flex-col items-center w-[200px] hover:bg-purple-500 hover:text-white transition duration-300">
-                            <i className="bi bi-house text-3xl"></i>
+                        <div className="bg-white shadow-lg rounded-lg p-4 flex flex-col items-center w-[150px] hover:bg-purple-500 hover:text-white transition duration-300">
+                            <i className="text-3xl bi bi-hearts"></i>
                             <p className="text-sm mt-2">{ capitalizeWords(DocumentType.BIRTH) }</p>
                         </div>
 
-                        <div className="bg-white shadow-lg rounded-lg p-6 flex flex-col items-center w-[200px] hover:bg-purple-500 hover:text-white transition duration-300">
-                            <i className="bi bi-gear text-3xl"></i>
+                        <div className="bg-white shadow-lg rounded-lg p-2 flex flex-col items-center w-[150px] hover:bg-purple-500 hover:text-white transition duration-300">
+                            <i className="bi bi-house-heart text-3xl mt-2"></i>
                             <p className="text-sm mt-2">{ capitalizeWords(DocumentType.MARRIAGE) }</p>
                         </div>
 
-                        <div className="bg-white shadow-lg rounded-lg p-6 flex flex-col items-center w-[200px] hover:bg-purple-500 hover:text-white transition duration-300">
-                            <i className="bi bi-person text-3xl"></i>
+                        <div className="bg-white shadow-lg rounded-lg p-2 flex flex-col items-center w-[150px] hover:bg-purple-500 hover:text-white transition duration-300">
+                            <i className="bi bi-heart-pulse text-3xl mt-2"></i>
                             <p className="text-sm mt-2">{ capitalizeWords(DocumentType.DEATH) }</p>
                         </div>
                     </div>
