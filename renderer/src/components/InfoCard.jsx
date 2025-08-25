af0457c2d339d8509cd822364bbf6e7d2fa00a67
Torrexx <fdc.pablitot@gmail.com>
2025-08-25 17:43:58 +0800

    pablito-dev: changed the info color to purple

--- renderer/src/components/InfoCard.jsx
+++ renderer/src/components/InfoCard.jsx
@@ -6,11 +6,11 @@ export default function InfoCard({ title = "Information", message = "This is an 
   if (!isVisible) return null; // Hide when closed
 
   return (
-    <div className="relative bg-blue-50 border border-blue-300 text-blue-800 p-4 rounded-xl shadow-sm w-full">
+    <div className="relative bg-purple-50 border border-purple-300 text-purple-800 p-4 rounded-xl shadow-sm w-full">
       {/* Close button */}
         <button
             onClick={() => setIsVisible(false)}
-            className="absolute top-2 right-2 text-blue-500 hover:text-blue-700 bg-blue-200  hover:bg-blue-150 rounded-full w-8 h-8 flex items-center justify-center shadow-sm focus:outline-none"
+            className="absolute top-2 right-2 text-purple-500 hover:text-purple-700 bg-purple-200  hover:bg-purple-150 rounded-full w-8 h-8 flex items-center justify-center shadow-sm focus:outline-none"
             >
             <i className="bi bi-x text-lg"></i>
         </button>
