6f5affd5f6c2c125668d29e543cef80f78508da3
10969torrexx <pablitojrtorre@gmail.com>
2025-08-26 22:30:58 +0800

    pablito-dev: create rules and validation for birth cert form

--- renderer/services/index.js
+++ renderer/services/index.js
@@ -1,1 +1,3 @@
 export * as AuthServices from './Auth/Services';
+export * as BirthCertificationServices from './BirthCertification/Services';
+export * as BirthCertificationValidation from './BirthCertification/Validation';
