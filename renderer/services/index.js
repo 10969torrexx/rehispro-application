cd4bd9efc117c4b8ecc7a5de2aee37583c6ed4a5
10969torrexx <pablitojrtorre@gmail.com>
2025-08-26 22:33:28 +0800

    pablito-dev: minor fix

--- renderer/services/index.js
+++ renderer/services/index.js
@@ -1,3 +1,3 @@
 export * as AuthServices from './Auth/Services';
-export * as BirthCertificationServices from './BirthCertification/Services';
-export * as BirthCertificationValidation from './BirthCertification/Validation';
+export * as BirthCertificationServices from './BirthCertificate/Services';
+export * as BirthCertificationValidation from './BirthCertificate/Validation';
