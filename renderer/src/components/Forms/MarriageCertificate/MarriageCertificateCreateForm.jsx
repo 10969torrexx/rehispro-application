6f01602f82c8592578112e6e38db8c20cf5b5dff
derekcatigan <derekcatigan03@gmail.com>
2025-09-06 10:19:24 +0800

    joshua-dev: Updated Create Marriage Cert Form and Installed Package for Signature

--- renderer/src/components/Forms/MarriageCertificate/MarriageCertificateCreateForm.jsx
+++ renderer/src/components/Forms/MarriageCertificate/MarriageCertificateCreateForm.jsx
@@ -1,10 +1,16 @@
-import { useState } from "react";
+import SignaturePad from 'react-signature-canvas';
+import { useState, useRef  } from "react";
 
 export default function MarriageCertificateCreateForm() {
     const [husbandBirthDate, setHusbandBirthDate] = useState("");
     const [husbandAge, setHusbandAge] = useState("");
     const [wifeBirthDate, setWifeBirthDate] = useState("");
     const [wifeAge, setWifeAge] = useState("");
+
+    // Handles the Pagination 
+    const [currentPage, setCurrentPage] = useState(1);
+    const totalPages = 14;
+
 
     const calculateAge = (birthDate) => {
         if (!birthDate) return "";
@@ -18,9 +24,6 @@ export default function MarriageCertificateCreateForm() {
         return age;
     };
 
-    const [currentPage, setCurrentPage] = useState(1);
-    const totalPages = 14;
-
     const handleHusbandBirthDate = (e) => {
         const value = e.target.value;
         setHusbandBirthDate(value);
@@ -32,6 +35,13 @@ export default function MarriageCertificateCreateForm() {
         setWifeBirthDate(value);
         setWifeAge(calculateAge(value));
     };
+
+    // Handles for Husband and Wife's Signature
+    const sigHusbandRef = useRef({});
+    const sigWifeRef = useRef({});
+
+    const clearHusband = () => sigHusbandRef.current.clear();
+    const clearWife = () => sigWifeRef.current.clear();
 
     return (
         <>
@@ -578,23 +588,236 @@ export default function MarriageCertificateCreateForm() {
                                 <input type="text" name="certYear" className="border-b border-gray-500 mx-2 w-16 text-center" />.
                             </p>
 
-
                             <div className="flex justify-evenly items-center mt-4">
-                                <div className="flex flex-col justify-center">
-                                    <input type="text" name="signatureHusband" className="w-full border-b border-gray-500 mx-2 text-center"/>
-                                    <label className="text-center">(Signature of Husband)</label>
+                                {/* Husband signature */}
+                                <div className="flex flex-col items-center">
+                                    <SignaturePad
+                                        ref={sigHusbandRef}
+                                        penColor="black"
+                                        canvasProps={{ width: 300, height: 100, className: "border border-gray-500 rounded" }}
+                                    />
+                                    <button type="button" onClick={clearHusband} className="text-xs mt-1 text-blue-500">Clear</button>
+                                    <label className="text-center mt-1">(Signature of Husband)</label>
                                 </div>
-                                <div className="flex flex-col justify-center">
-                                    <input type="text" name="signatureWife" className="w-full border-b border-gray-500 mx-2 text-center"/>
-                                    <label className="text-center">(Signature of Wife)</label>
+
+                                {/* Wife signature */}
+                                <div className="flex flex-col items-center">
+                                    <SignaturePad
+                                        ref={sigWifeRef}
+                                        penColor="black"
+                                        canvasProps={{ width: 300, height: 100, className: "border border-gray-500 rounded" }}
+                                    />
+                                    <button type="button" onClick={clearWife} className="text-xs mt-1 text-blue-500">Clear</button>
+                                    <label className="text-center mt-1">(Signature of Wife)</label>
                                 </div>
                             </div>
                         </div>
                     </>
                 )}
 
+                {currentPage === 5 && (
+                    <>
+                        {/* Certification of Solemnizing Officer */}
+                        <span>
+                        <p>19. Certification of Solemnizing Officer</p>
+                        </span>
 
-            </form >
+                        <div className="mt-1 mb-3 text-sm leading-relaxed border border-pink-300 p-3 rounded">
+                        <p>
+                        THIS IS TO CERTIFY: THAT BEFORE ME, on the date and place above-written,
+                        personally appeared the above-mentioned parties, with their mutual consent,
+                        lawfully joined together in marriage which was solemnized by me in the presence
+                        of the witnesses named below, all of legal age.
+                        </p>
+
+                        <p className="mt-3 pl-6 font-semibold">I CERTIFY FURTHER THAT:</p>
+
+                        {/* Options a, b, c */}
+                        <div className="mt-2 space-y-2 pl-10">
+                        <label className="flex items-center gap-2">
+                            <input type="checkbox" name="certification" value="license" />
+                            a. Marriage License No.{" "}
+                            <input
+                            type="text"
+                            name="marriageLicenseNo"
+                            className="border-b border-gray-500 w-32 text-center mx-1"
+                            />{" "}
+                            issued on{" "}
+                            <input
+                            type="text"
+                            name="marriageIssuedOn"
+                            className="border-b border-gray-500 w-32 text-center mx-1"
+                            />{" "}
+                            at{" "}
+                            <input
+                            type="text"
+                            name="marriageIssuedAt"
+                            className="border-b border-gray-500 w-48 text-center mx-1"
+                            />{" "}
+                            in favor of said parties, was exhibited to me.
+                        </label>
+
+                        <label className="flex items-center gap-2">
+                            <input type="checkbox" name="certification" value="noLicense" />
+                            b. No marriage license was necessary, the marriage being solemnized under Art.{" "}
+                            <input
+                            type="text"
+                            name="executiveOrder"
+                            className="border-b border-gray-500 w-16 text-center mx-1"
+                            />{" "}
+                            of Executive Order No. 209.
+                        </label>
+
+                        <label className="flex items-center gap-2">
+                            <input type="checkbox" name="certification" value="pd1083" />
+                            c. The marriage was solemnized in accordance with the provisions of Presidential Decree No. 1083.
+                        </label>
+                        </div>
+
+                        {/* Signature fields */}
+                        <div className="flex justify-evenly items-center mt-6">
+                        <div className="flex flex-col items-center">
+                            <input
+                            type="text"
+                            name="solemnizingOfficerName"
+                            className="w-56 border-b border-gray-500 text-center"
+                            />
+                            <label className="text-xs text-center mt-1">
+                            (Signature Over Printed Name of Solemnizing Officer)
+                            </label>
+                        </div>
+
+                        <div className="flex flex-col items-center">
+                            <input
+                            type="text"
+                            name="officerPosition"
+                            className="w-40 border-b border-gray-500 text-center"
+                            />
+                            <label className="text-xs text-center mt-1">(Position/Designation)</label>
+                        </div>
+
+                        <div className="flex flex-col items-center">
+                            <input
+                            type="text"
+                            name="officerReligion"
+                            className="w-56 border-b border-gray-500 text-center"
+                            />
+                            <label className="text-xs text-center mt-1">
+                            (Religion/Religious Sect, Registry No. and Expiration Date, if applicable)
+                            </label>
+                        </div>
+                        </div>
+                        </div>
+
+                        {/* 20a. Witnesses */}
+                        <span>
+                        <p>20a. Witnesses (Print Name and Sign)</p>
+                        </span>
+
+                        <div className="border border-pink-300 p-3 rounded mb-3">
+                        <div className="grid grid-cols-2 gap-6">
+                            <div className="flex flex-col items-center">
+                            <input
+                                type="text"
+                                name="witness1Signature"
+                                className="w-56 border-b border-gray-500 text-center"
+                            />
+                            <label className="text-xs mt-1">(Signature of Witness 1)</label>
+                            <input
+                                type="text"
+                                name="witness1Name"
+                                placeholder="Name in Print"
+                                className="w-56 border-b border-gray-500 text-center mt-2"
+                            />
+                            </div>
+
+                            <div className="flex flex-col items-center">
+                            <input
+                                type="text"
+                                name="witness2Signature"
+                                className="w-56 border-b border-gray-500 text-center"
+                            />
+                            <label className="text-xs mt-1">(Signature of Witness 2)</label>
+                            <input
+                                type="text"
+                                name="witness2Name"
+                                placeholder="Name in Print"
+                                className="w-56 border-b border-gray-500 text-center mt-2"
+                            />
+                            </div>
+                        </div>
+                        </div>
+                    </>
+                )}
+
+                {currentPage === 6 && (
+                    <>
+                        {/* 21. Received By */}
+                        <span>
+                            <p>21. Received By</p>
+                        </span>
+                        <div className="border border-pink-300 p-3 rounded mb-3">
+                            <div className="flex flex-col items-center space-y-2">
+                                <input
+                                type="text"
+                                name="receivedBySignature"
+                                placeholder="Signature"
+                                className="w-56 border-b border-gray-500 text-center"
+                                />
+                                <input
+                                type="text"
+                                name="receivedByName"
+                                placeholder="Name in Print"
+                                className="w-56 border-b border-gray-500 text-center"
+                                />
+                                <input
+                                type="text"
+                                name="receivedByTitle"
+                                placeholder="Title or Position"
+                                className="w-56 border-b border-gray-500 text-center"
+                                />
+                                <input
+                                type="date"
+                                name="receivedByDate"
+                                className="w-56 border-b border-gray-500 text-center"
+                                />
+                            </div>
+                        </div>
+
+                        {/* 22. Registered by the Civil Registrar */}
+                        <span>
+                            <p>22. Registered by the Civil Registrar</p>
+                        </span>
+                        <div className="border border-pink-300 p-3 rounded mb-3">
+                            <div className="flex flex-col items-center space-y-2">
+                                <input
+                                type="text"
+                                name="registrarSignature"
+                                placeholder="Signature"
+                                className="w-56 border-b border-gray-500 text-center"
+                                />
+                                <input
+                                type="text"
+                                name="registrarName"
+                                placeholder="Name in Print"
+                                className="w-56 border-b border-gray-500 text-center"
+                                />
+                                <input
+                                type="text"
+                                name="registrarTitle"
+                                placeholder="Title or Position"
+                                className="w-56 border-b border-gray-500 text-center"
+                                />
+                                <input
+                                type="date"
+                                name="registrarDate"
+                                className="w-56 border-b border-gray-500 text-center"
+                                />
+                            </div>
+                        </div>
+                    </>
+                )}
+            </form>
 
             <div className="flex justify-center items-center gap-4">
                 {/* Previous Button */}
