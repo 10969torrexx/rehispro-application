e90400f69a58c1a52c37865130e33c7b6d438736
Torrexx <83467428+10969torrexx@users.noreply.github.com>
2025-09-24 07:10:15 +0800

    torrexx-dev: use dropzone for upload file

--- renderer/src/components/Forms/BirthCertificate/BIrthCertificateUpload.jsx
+++ renderer/src/components/Forms/BirthCertificate/BIrthCertificateUpload.jsx
@@ -1,15 +1,22 @@
-import { Divider } from '@components';
+import React, { useCallback } from "react"
+import { useDropZone } from "react-dropzone";
+
 export default function Upload() { 
+    const onDrop = useCallback(uploadedFiles => {
+        console.log(uploadedFiles);
+    }, []);
+    const {getRootProps, getInputProps, isDragActive} = useDropZone({onDrop});
+
     return (
-        <div className="w-full flex">
-            <Divider />
-            <div className="w-full border-dashed border rounded-lg border-2 border-gray-300 h-64 flex flex-col justify-center items-center text-gray-400">
-                <div className='p-4 rounded-full'>
-                    <i className="text-4xl fa-solid fa-cloud-arrow-up"></i>
-                </div>
-                <div>Choose or drag and drop files here to upload</div>
-                <button className='mt-4 rounded-full px-4 py-2 btn-primary'>Choose File(s)</button>
+        <div {...getRootProps()} className="w-full border-dashed border rounded-lg border-2 border-gray-300 h-64 flex flex-col justify-center items-center text-gray-400">
+            <input {...getInputProps()} />
+            <div className='p-4 rounded-full'>
+                <i className="text-4xl fa-solid fa-cloud-arrow-up"></i>
             </div>
+            <div>
+                {isDragActive ? <p>Drop the files here ...</p> : <p>Choose or drag and drop files here to upload</p>}
+            </div>
+            <button className='mt-4 rounded-full px-4 py-2 btn-primary'>Browse Files</button>
         </div>
     )
 }
